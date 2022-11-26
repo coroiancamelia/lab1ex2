4902e137451e92c45097662622a484b63cb68c65
coroiancamelia <coroiancamelia@outlook.com>
2022-11-26 20:40:45 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 2;
+
+            int b = 4;
+
+            int c = 6;
+
+            int mediaAritmetica = (a + b + c) / 3;
+
+
+            Console.WriteLine(mediaAritmetica);
+
+        }
+    }
+}
